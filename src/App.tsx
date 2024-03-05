e94f5a2cab61287541627bec4178472d3d8ef71a
cristianaquino <ycristian10@gmail.com>
2024-03-05 17:40:42 -0500

    feat: change routes for access to projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import { RoutesWithNotFound } from "@guards/index";
-import { PUBLIC_ROUTE } from "@routes/index";
-import { lazy } from "react";
+import { PROYECTS_ROUTE } from "@routes/index";
+import { Suspense, lazy } from "react";
 import "@app/App.css";
+import { Loading } from "@components/Loading";
 
-const Proyects = lazy(() => import("@pages/Proyects/Proyects"));
-const Components = lazy(() => import("@pages/Components/Components"));
+const Ecommerce = lazy(() => import("@pages/Ecommerce/Ecommerce"));
+const Gifs = lazy(() => import("@pages/Gifs/Gifs"));
 
 function App() {
   return (
     <BrowserRouter>
-      <RoutesWithNotFound pageRedirect={PUBLIC_ROUTE.HOME}>
-        <Route path={PUBLIC_ROUTE.HOME} element={<div>Home</div>} />
-        {/* anadimos el * para que cualquier ruta acceda mediante proyects/[page_name] */}
-        <Route path={`${PUBLIC_ROUTE.PROYECTS}/*`} element={<Proyects />} />
-        <Route path={`${PUBLIC_ROUTE.COMPONENTS}/*`} element={<Components />} />
+      <RoutesWithNotFound message="Proyect not found">
+        <Route
+          path={`${PROYECTS_ROUTE.ECOMMERCE}/*`}
+          element={
+            <Suspense fallback={<Loading />}>
+              <Ecommerce />
+            </Suspense>
+          }
+        />
+        <Route
+          path={`${PROYECTS_ROUTE.GIFS}/*`}
+          element={
+            <Suspense fallback={<Loading />}>
+              <Gifs />
+            </Suspense>
+          }
+        />
       </RoutesWithNotFound>
     </BrowserRouter>
   );
