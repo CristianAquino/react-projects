20c90f491b3f17f58c176a836eefb877bfd6cbe9
cristianaquino <ycristian10@gmail.com>
2024-03-09 00:16:54 -0500

    feat: routes for page teacher

--- src/pages/Teacher/helpers/route.helper.ts
+++ src/pages/Teacher/helpers/route.helper.ts
@@ -1,31 +1,30 @@
 const TEACHER_ENDPOINT = {
-  POST_LOGIN: "teacher/login/",
-  POST_REGISTER: "teacher/add/",
-  GET_ME: "teacher/me/",
-  PUT_ME: "teacher/me/",
+  POST_TEACHER_LOGIN: "teacher/login/",
+  POST_TEACHER_REGISTER: "teacher/add/",
+  TEACHER_ME: "teacher/me/",
 };
 const COURSE_ENDPOINT = {
-  GET_ONE_BY_ID: "course/",
-  GET_LIST: "course/list/",
-  POST_CREATE: "course/create/",
-  PUT_UPDATE_BY_ID: "course/update/",
-  DELETE_ONE_BY_ID: "course/delete/",
+  GET_COURSE_ONE_BY_ID: "course/",
+  GET_COURSE_LIST: "course/list/",
+  POST_COURSE_CREATE: "course/create/",
+  PUT_COURSE_UPDATE_BY_ID: "course/update/",
+  DELETE_COURSE_ONE_BY_ID: "course/delete/",
 };
 const STUDENT_ENDPOINT = {
-  GET_ONE_BY_ID: "student/one/",
-  POST_CREATE_BY_COURSE_ID: "student/create/",
-  PUT_UPDATE_BY_ID: "student/update/",
-  DELETE_ONE_BY_ID: "student/delete/",
+  GET_STUDENT_ONE_BY_ID: "student/one/",
+  POST_STUDENT_CREATE_BY_COURSE_ID: "student/create/",
+  PUT_STUDENT_UPDATE_BY_ID: "student/update/",
+  DELETE_STUDENT_ONE_BY_ID: "student/delete/",
 };
 const CALIFICATION_ENDPOINT = {
-  GET_LIST_BY_COURSEID: "calification/list/",
-  POST_CREATE: "calification/create/",
-  PUT_UPDATE: "calification/update/",
+  GET_CALIFICATION_LIST_BY_COURSEID: "calification/list/",
+  POST_CALIFICATION_CREATE: "calification/create/",
+  PUT_CALIFICATION_UPDATE: "calification/update/",
 };
 const ATTENDANCE_ENDPOINT = {
-  GET_LIST_BY_COURSEID: "attendance/list/",
-  POST_CREATE: "attendance/create/",
-  PUT_UPDATE_BY_ID: "attendance/update/",
+  GET_ATTENDANCE_LIST_BY_COURSEID: "attendance/list/",
+  POST_ATTENDANCE_CREATE: "attendance/create/",
+  PUT_ATTENDANCE_UPDATE_BY_ID: "attendance/update/",
 };
 
 const TEACHER_PAGE = {};
