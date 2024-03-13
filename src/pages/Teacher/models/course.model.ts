fa738a4c807176cad84b198e8ace98fe05e1a882
cristianaquino <ycristian10@gmail.com>
2024-03-13 15:40:31 -0500

    feat: course model and schema

--- src/pages/Teacher/models/course.model.ts
+++ src/pages/Teacher/models/course.model.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
-import { CreateCourseSchema } from ".";
+import { CreateCourseSchema, ListCourseSchema, MeCourseSchema } from ".";
 
 export type CreateCourseType = z.infer<typeof CreateCourseSchema>;
+export type ListCourseType = z.infer<typeof ListCourseSchema>;
+export type MeCourseType = z.infer<typeof MeCourseSchema>;
--- src/pages/Teacher/models/schemas/course.schema.ts
+++ src/pages/Teacher/models/schemas/course.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
-import { Level } from "..";
+import { BaseStudentDataSchema, Level } from "..";
 
-export const CreateCourseSchema = z.object({
+const CreateCourseSchema = z.object({
   name: z
     .string({ required_error: "name is required" })
     .trim()
@@ -10,12 +10,50 @@ export const CreateCourseSchema = z.object({
     .regex(/^[a-zA-Z\s]+$/gi, { message: "invalid name" }),
   level: z.nativeEnum(Level),
   degree: z
-    .string({ required_error: "degree is required" })
-    .trim()
-    .regex(/^[1-6]$/gi, { message: "invalid degree" }),
+    .number({ required_error: "degree is required" })
+    .lte(6, { message: "max degree must be 6" })
+    .gte(1, { message: "min degree must be 1" }),
   section: z
     .string({ required_error: "section is required" })
     .trim()
     .max(1)
-    .regex(/^([A-U]|\d)$/gi, { message: "invalid section" }),
+    .regex(/^([A-U]|\d)$/, { message: "invalid section" }),
 });
+
+const ListCourseSchema = z.object({
+  courses: z.array(
+    z.object({
+      id: z
+        .string({
+          invalid_type_error: "id must be a string",
+        })
+        .trim()
+        .uuid({ message: "invalid format" }),
+      ...CreateCourseSchema.shape,
+    })
+  ),
+});
+
+const MeCourseSchema = z.object({
+  course: z.object({
+    id: z
+      .string({
+        invalid_type_error: "id must be a string",
+      })
+      .trim()
+      .uuid({ message: "invalid format" }),
+    ...CreateCourseSchema.shape,
+  }),
+  students: z.array(
+    z.object({
+      id: z
+        .string({
+          invalid_type_error: "id must be a string",
+        })
+        .trim()
+        .uuid({ message: "invalid format" }),
+      ...BaseStudentDataSchema,
+    })
+  ),
+});
+export { CreateCourseSchema, ListCourseSchema, MeCourseSchema };
