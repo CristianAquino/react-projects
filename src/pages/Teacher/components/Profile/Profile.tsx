9497bf9d8a16b444f8c05ed31eec8111af0cde4a
cristianaquino <ycristian10@gmail.com>
2024-03-12 20:42:32 -0500

    feat: teacher profile

--- src/pages/Teacher/components/Profile/Profile.tsx
+++ src/pages/Teacher/components/Profile/Profile.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import {
-  ErrorBoundaries,
-  getLocalStorage,
-  setLocalStorage,
-} from "@app/helpers";
+import { removeLocalStorage } from "@app/helpers";
 import { useEffect, useState } from "react";
 import { useFetchAndLoad } from "../../hooks";
 import { get_teacher_me } from "../../services";
@@ -15,25 +11,51 @@ export type ProfileProps = {
 
 const Profile = ({}: ProfileProps) => {
   const { callEndpoint } = useFetchAndLoad();
-  const [me, setMe] = useState(getLocalStorage({ key: "user" }));
+  const [account, setAccount] = useState<any>();
 
   useEffect(() => {
     async function getMe() {
       const { data } = await callEndpoint(get_teacher_me());
       if (data) {
-        setLocalStorage({ key: "user", value: data });
-        setMe(data);
+        setAccount(data);
       }
     }
     getMe();
     return () => {
-      setMe(null);
+      setAccount(null);
+      removeLocalStorage({ key: "user" });
     };
   }, []);
   return (
-    <ErrorBoundaries fallBackComponent={<p>Upps</p>} resetCondition={me}>
-      <div>{me}</div>
-    </ErrorBoundaries>
+    <div>
+      <h2>Teacher</h2>
+      <p>Names: {account?.user.name}</p>
+      <p>First Name: {account?.user.first_name}</p>
+      <p>Second Name: {account?.user.second_name}</p>
+      <p>Email: {account?.user.email}</p>
+      <h2>Courses</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Level</th>
+            <th>Degree</th>
+            <th>Section</th>
+          </tr>
+        </thead>
+        <tbody>
+          {account?.course?.map((course: any) => (
+            <tr key={course.id}>
+              <td>{course.name}</td>
+              <td>{course.level}</td>
+              <td>{course.degree}</td>
+              <td>{course.section}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <pre>{JSON.stringify(account, null, 2)}</pre>
+    </div>
   );
 };
 
