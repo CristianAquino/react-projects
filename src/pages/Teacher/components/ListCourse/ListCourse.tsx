6eb295a66b9b77eb1c2fbd95953abac39d01a6a9
cristianaquino <ycristian10@gmail.com>
2024-03-16 00:10:53 -0500

    feat: list course component

--- src/pages/Teacher/components/ListCourse/ListCourse.tsx
+++ src/pages/Teacher/components/ListCourse/ListCourse.tsx
@@ -5,6 +5,7 @@ import { LIST_COURSE } from "../../helpers";
 import { useFetchAndLoad } from "../../hooks";
 import { ListCourseType } from "../../models";
 import { get_course_list } from "../../services";
+import { TableData } from "..";
 
 export type ListCourseProps = {
   // types...
@@ -27,30 +28,7 @@ const ListCourse = ({}: ListCourseProps) => {
   return (
     <div>
       <h2>List of Courses</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Level</th>
-            <th>Degree</th>
-            <th>Section</th>
-          </tr>
-        </thead>
-        <tbody>
-          {courses.length > 0 ? (
-            courses.map((course: any) => (
-              <tr key={course.id}>
-                <td>{course.name}</td>
-                <td>{course.level}</td>
-                <td>{course.degree}</td>
-                <td>{course.section}</td>
-              </tr>
-            ))
-          ) : (
-            <p>No courses</p>
-          )}
-        </tbody>
-      </table>
+      <TableData datos={courses} type="course" />
     </div>
   );
 };
