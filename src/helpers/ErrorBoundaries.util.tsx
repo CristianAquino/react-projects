3b509a59b3347c94bc572322a87e1c23c7efcf08
cristianaquino <ycristian10@gmail.com>
2024-03-16 00:08:28 -0500

    feat: error boundary

---
+++ src/helpers/ErrorBoundaries.util.tsx
@@ -0,0 +1,37 @@
+import { Component, ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  fallBackComponent: ReactNode;
+  resetCondition?: any;
+}
+
+interface State {
+  hasError: boolean;
+  resetCondition?: any;
+}
+
+export class ErrorBoundaries extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false, resetCondition: props.resetCondition };
+  }
+
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true };
+  }
+
+  static getDerivedStateFromProps(props: Props, state: State) {
+    if (props.resetCondition !== state.resetCondition) {
+      return { hasError: false, resetCondition: props.resetCondition };
+    }
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallBackComponent;
+    }
+
+    return this.props.children;
+  }
+}
