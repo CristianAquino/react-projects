3c7d4baacfb179cbd74379809f4f69f746dd0050
cristianaquino <ycristian10@gmail.com>
2024-03-16 00:09:16 -0500

    feat: home component

--- src/pages/Teacher/components/Dashboard/Home/Home.tsx
+++ src/pages/Teacher/components/Dashboard/Home/Home.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import { Outlet } from "react-router-dom";
+
 export type HomeProps = {
   // types...
 };
 
 const Home = ({}: HomeProps) => {
   return (
-    <div>
-      <h1>Califiaction</h1>
-      <h1>Attendance</h1>
+    <div style={{ width: "70%", height: "100%" }}>
+      <Outlet />
     </div>
   );
 };
