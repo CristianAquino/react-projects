a1a6fe73d78fc71dfa9ec7bba3f39e62eab9284d
cristianaquino <ycristian10@gmail.com>
2024-03-27 22:21:17 -0500

    feat: add search for courses

--- src/pages/Teacher/components/ActionsInCourses/ActionsInCourses.tsx
+++ src/pages/Teacher/components/ActionsInCourses/ActionsInCourses.tsx
@@ -1,48 +1,63 @@
 "use client";
 
 import { getLocalStorage } from "@app/helpers";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useFetchAndLoad } from "../../hooks";
 import { ListCourseType } from "../../models";
+import { get_course_list } from "../../services";
 import { Container, Title } from "../Profile/styled-components";
-import { TableData } from "..";
+import { LabelSheet } from "../RegisterStudents/styled-components";
+import { TableDataCourseUpdate } from "../TableData/TableData";
 
 export type ActionsInCoursesProps = {
   // types...
 };
 
 const ActionsInCourses = ({}: ActionsInCoursesProps) => {
-  // const { callEndpoint } = useFetchAndLoad();
+  const { callEndpoint } = useFetchAndLoad();
+  const [search, setSearch] = useState("");
   const course = getLocalStorage({ key: "courses" });
-  const [{ courses }, _] = useState<ListCourseType>({
+  const [{ courses }, setAccount] = useState<ListCourseType>({
     courses: course,
   });
 
-  // useEffect(() => {
-  //   async function get_list_course() {
-  //     if (!course) {
-  //       const { data } = await callEndpoint(get_course_list());
-  //       if (data) {
-  //         setAccount(data);
-  //       }
-  //     } else {
-  //       setAccount({ courses: course });
-  //     }
-  //   }
-  //   get_list_course();
-  // }, []);
+  useEffect(() => {
+    async function get_list_course() {
+      if (!course) {
+        const { data } = await callEndpoint(get_course_list());
+        if (data) {
+          setAccount(data);
+        }
+      } else {
+        setAccount({ courses: course });
+      }
+    }
+    get_list_course();
+  }, []);
 
   return (
     <Container>
       <Title>list of courses</Title>
-      {/* <p>
-        Select or search for a course for which you want to edit your
-        information
-      </p>
-      <label aria-label="insert the name of the course to search">
-        <input type="text" />
-      </label> */}
-      {/* agrega un boton a la tabla de acciones como editar y borrar */}
-      <TableData datos={courses} type="course-update" />
+      <LabelSheet>
+        <span>
+          Select or search for a course for which you want to edit your
+          information
+        </span>
+        <input
+          type="text"
+          value={search}
+          aria-label="insert the name of the course to search"
+          onChange={(e) => setSearch(e.target.value)}
+          autoFocus
+        />
+      </LabelSheet>
+      <TableDataCourseUpdate
+        url={"/teacher/dashboard/course/info"}
+        datos={courses.filter((e: any) => {
+          if (search == "") return courses;
+          return e.name.toLowerCase().includes(search.toLowerCase());
+        })}
+      />
     </Container>
   );
 };
