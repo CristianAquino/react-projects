eb45a7f8056a9ef1f66a3d8fa8d134160cb1e819
cristianaquino <ycristian10@gmail.com>
2024-04-08 14:42:10 -0500

    feat: tables

--- src/pages/Teacher/components/TableData/TableData.tsx
+++ src/pages/Teacher/components/TableData/TableData.tsx
@@ -1,32 +1,15 @@
 "use client";
 
-import { useRef, useState } from "react";
-import { UPDATE_COURSE } from "../../helpers";
-import { useFetchAndLoad, useValidateForm } from "../../hooks";
 import {
   CourseType,
-  CreateCourseSchema,
   CreateStudentType,
   GetListCourseType,
   InitialAttendanceType,
   InitialCalificationType,
   MeStudentType,
 } from "../../models";
-import { useCourseStorage } from "../../store";
 import {
-  InputButtons,
-  LabelError,
-  Label as LabelU,
-  LoadingForm,
-} from "../Login/styled-components";
-import { Modal } from "../Modal";
-import { Container, Data, Label, Title } from "../Profile/styled-components";
-import {
-  FormCourse,
-  Option,
-  Select,
-} from "../RegisterCourse/styled-components";
-import {
+  LabelInput,
   LabelTD,
   LinkData,
   Table,
@@ -85,7 +68,6 @@ export type TableDataStudentInfoProps = {
   datos: MeStudentType[];
   url: string;
 };
-
 export const TableDataStudentInfo = ({
   datos,
   url,
@@ -207,261 +189,86 @@ export const TableDataCourseUpload = ({
   );
 };
 
-export const TableDataCourseUpdate = ({ datos, url }: TableDataCourseProps) => {
+export type TableDataEditCourseProps = {
+  datos: GetListCourseType;
+  url: string;
+  handleUpdateOpen: (data: CourseType) => void;
+  handleDeleteOpen: (data: CourseType) => void;
+};
+export const TableDataEditCourse = ({
+  datos,
+  url,
+  handleUpdateOpen,
+  handleDeleteOpen,
+}: TableDataEditCourseProps) => {
   const head = ["#", "name", "level", "degree", "section", "actions"];
-  const { callEndpoint, loading } = useFetchAndLoad();
-  const modalDeleteRef = useRef<HTMLDialogElement>(null);
-  const modalUpdateRef = useRef<HTMLDialogElement>(null);
-  const { setDeleteCourse, setUpdateCourse } = useCourseStorage((state) => ({
-    setDeleteCourse: state.setDeleteCourse,
-    setUpdateCourse: state.setUpdateCourse,
-  }));
-  const [modalCourse, setModalCourse] = useState<CourseType>(UPDATE_COURSE);
-  const [degree, setDegree] = useState<number[]>([1, 2, 3, 4, 5, 6]);
-  const { errors, flag } = useValidateForm({
-    schema: CreateCourseSchema,
-    data: modalCourse,
-  });
-
-  function handleDeleteOpen(data: any) {
-    modalDeleteRef.current?.showModal();
-    setModalCourse(data);
-  }
-  function handleDeleteClose() {
-    modalDeleteRef.current?.close();
-  }
-  async function handleDelete() {
-    // await callEndpoint(delete_course_id({ id: modalCourse.id }));
-    setDeleteCourse(modalCourse.id);
-  }
-
-  function handleUpdateOpen(data: CourseType) {
-    modalUpdateRef.current?.showModal();
-    setModalCourse(data);
-  }
-  function handleUpdateClose() {
-    modalUpdateRef.current?.close();
-  }
-  async function handleUpdate(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    // await callEndpoint(
-    //   put_course_id({ data: modalCourse, id: modalCourse.id })
-    // );
-    setUpdateCourse(modalCourse);
-  }
-  const handleChange = (e: React.ChangeEvent<HTMLFormElement>) => {
-    if (e.target.value === "primaria") {
-      setDegree([1, 2, 3, 4, 5, 6]);
-    } else if (e.target.value === "secundaria") {
-      setDegree([1, 2, 3, 4, 5]);
-    }
-    if (e.target.name === "section") {
-      return setModalCourse({
-        ...modalCourse,
-        section: e.target.value.toUpperCase(),
-      });
-    }
-    setModalCourse({ ...modalCourse, [e.target.name]: e.target.value });
-  };
-
   return (
-    <>
-      {/* modal delete */}
-      <Modal ref={modalDeleteRef}>
-        <Title>course info to delete</Title>
-        <Data>
-          <Label>
-            <span>course name: </span>
-            <span>{modalCourse.name}</span>
-          </Label>
-          <Label>
-            <span>level: </span>
-            <span>{modalCourse.level}</span>
-          </Label>
-          <Label>
-            <span>degree: </span>
-            <span>{modalCourse.degree}</span>
-          </Label>
-          <Label>
-            <span>section: </span>
-            <span>{modalCourse.section}</span>
-          </Label>
-          <LabelError style={{ marginBlockStart: ".5rem" }}>
-            Are you sure you want to delete this course?
-          </LabelError>
-          <InputButtons>
-            <button
-              onClick={handleDelete}
-              style={{ backgroundColor: "#1567ff" }}
-            >
-              confirm
-            </button>
-            <button onClick={handleDeleteClose} type="button">
-              cancel
-            </button>
-          </InputButtons>
-          {loading && (
-            <LoadingForm>
-              <p>loading...</p>
-            </LoadingForm>
-          )}
-        </Data>
-      </Modal>
-      {/* modal update */}
-      <Modal ref={modalUpdateRef}>
-        <Title>course info to update</Title>
-        <Container style={{ inlineSize: "64%" }}>
-          <FormCourse onSubmit={handleUpdate} onChange={handleChange}>
-            <LabelU aria-label="enter the name of the course">
-              <span>name course:</span>
-              <input
-                type="text"
-                name="name"
-                value={modalCourse.name}
-                autoFocus
-              />
-            </LabelU>
-            {errors?.name && (
-              <div>
-                {errors.name.map((error) => (
-                  <LabelError key={error}>{error}</LabelError>
-                ))}
-              </div>
-            )}
-            <LabelU aria-label="select course level">
-              <span>level:</span>
-              <Select name="level" value={modalCourse.level}>
-                <Option value="primaria">primaria</Option>
-                <Option value="secundaria">secundaria</Option>
-              </Select>
-            </LabelU>
-            {errors?.level && (
-              <div>
-                {errors.level.map((error) => (
-                  <LabelError key={error}>{error}</LabelError>
-                ))}
-              </div>
-            )}
-            <LabelU aria-label="select course level">
-              <span>degree:</span>
-              <Select name="degree" value={modalCourse.degree}>
-                {degree.map((e) => (
-                  <Option key={e} value={e}>
-                    {e}
-                  </Option>
-                ))}
-              </Select>
-            </LabelU>
-            {errors?.degree && (
-              <div>
-                {errors.degree.map((error) => (
-                  <LabelError key={error}>{error}</LabelError>
-                ))}
-              </div>
-            )}
-            <LabelU aria-label="enter the section of the course">
-              <span>section:</span>
-              <input type="text" name="section" value={modalCourse.section} />
-            </LabelU>
-            {errors?.section && (
-              <div>
-                {errors.section.map((error) => (
-                  <LabelError key={error}>{error}</LabelError>
-                ))}
-              </div>
-            )}
-            <LabelError style={{ marginBlockStart: ".5rem" }}>
-              Are you sure you want to update this course?
-            </LabelError>
-            <InputButtons>
-              <input type="submit" value="Confirm" disabled={flag} />
-              <button
-                onClick={handleUpdateClose}
-                type="button"
-                formMethod="dialog"
+    <TableData>
+      <thead>
+        <Tr>
+          {head.map((item, index) => (
+            <Th key={index}>{item}</Th>
+          ))}
+        </Tr>
+      </thead>
+      <tbody>
+        {datos.length > 0 ? (
+          datos.map((course, index) => (
+            <Tr key={course.id}>
+              <Td>{index + 1}</Td>
+              <Td>
+                <LinkData to={`${url}/${course.id}`}>{course.name}</LinkData>
+              </Td>
+              <Td>
+                <LinkData to={`${url}/${course.id}`}>{course.level}</LinkData>
+              </Td>
+              <Td>
+                <LinkData to={`${url}/${course.id}`}>{course.degree}</LinkData>
+              </Td>
+              <Td>
+                <LinkData to={`${url}/${course.id}`}>{course.section}</LinkData>
+              </Td>
+              <Td
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  gap: "1rem",
+                }}
               >
-                cancel
-              </button>
-            </InputButtons>
-          </FormCourse>
-          {loading && (
-            <LoadingForm>
-              <p>loading...</p>
-            </LoadingForm>
-          )}
-        </Container>
-      </Modal>
-      <TableData>
-        <thead>
-          <Tr>
-            {head.map((item, index) => (
-              <Th key={index}>{item}</Th>
-            ))}
-          </Tr>
-        </thead>
-        <tbody>
-          {datos.length > 0 ? (
-            datos.map((course, index) => (
-              <Tr key={course.id}>
-                <Td>{index + 1}</Td>
-                <Td>
-                  <LinkData to={`${url}/${course.id}`}>{course.name}</LinkData>
-                </Td>
-                <Td>
-                  <LinkData to={`${url}/${course.id}`}>{course.level}</LinkData>
-                </Td>
-                <Td>
-                  <LinkData to={`${url}/${course.id}`}>
-                    {course.degree}
-                  </LinkData>
-                </Td>
-                <Td>
-                  <LinkData to={`${url}/${course.id}`}>
-                    {course.section}
-                  </LinkData>
-                </Td>
-                <Td
+                <button
                   style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    gap: "1rem",
+                    backgroundColor: "#0d4dff",
+                    border: "none",
+                    padding: ".25rem .5rem",
+                    borderRadius: ".5rem",
+                    textTransform: "capitalize",
+                    cursor: "pointer",
                   }}
+                  onClick={() => handleUpdateOpen(course)}
                 >
-                  <button
-                    style={{
-                      backgroundColor: "#0d4dff",
-                      border: "none",
-                      padding: ".25rem .5rem",
-                      borderRadius: ".5rem",
-                      textTransform: "capitalize",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => handleUpdateOpen(course)}
-                  >
-                    update
-                  </button>
-                  <button
-                    style={{
-                      backgroundColor: "#ea0000",
-                      border: "none",
-                      padding: ".25rem .5rem",
-                      borderRadius: ".5rem",
-                      textTransform: "capitalize",
-                      cursor: "pointer",
-                    }}
-                    onClick={() => handleDeleteOpen(course)}
-                  >
-                    delete
-                  </button>
-                </Td>
-              </Tr>
-            ))
-          ) : (
-            <p>No courses</p>
-          )}
-        </tbody>
-      </TableData>
-    </>
+                  update
+                </button>
+                <button
+                  style={{
+                    backgroundColor: "#ea0000",
+                    border: "none",
+                    padding: ".25rem .5rem",
+                    borderRadius: ".5rem",
+                    textTransform: "capitalize",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleDeleteOpen(course)}
+                >
+                  delete
+                </button>
+              </Td>
+            </Tr>
+          ))
+        ) : (
+          <p>No courses</p>
+        )}
+      </tbody>
+    </TableData>
   );
 };
 
@@ -469,7 +276,6 @@ export type TableTakeAttendanceStudentType = {
   datos: InitialAttendanceType;
   setAttendance: (st: any) => void;
 };
-
 export const TableTakeAttendanceStudent = ({
   datos,
   setAttendance,
@@ -575,16 +381,8 @@ export const TableSaveCalificationStudent = ({
                   }`,
                 }}
               >
-                <LabelU
-                  style={{
-                    justifyContent: "center",
-                    fontSize: "initial",
-                  }}
-                >
+                <LabelInput>
                   <input
-                    style={{
-                      marginInlineStart: "0",
-                    }}
                     type="text"
                     name={`pt${student.id}`}
                     value={student.pt}
@@ -592,7 +390,7 @@ export const TableSaveCalificationStudent = ({
                       setAttendance({ ...student, pt: e.target.value })
                     }
                   />
-                </LabelU>
+                </LabelInput>
               </TdInput>
               <TdInput
                 style={{
@@ -603,11 +401,8 @@ export const TableSaveCalificationStudent = ({
                   }`,
                 }}
               >
-                <LabelU
-                  style={{ justifyContent: "center", fontSize: "initial" }}
-                >
+                <LabelInput>
                   <input
-                    style={{ marginInlineStart: "0" }}
                     type="text"
                     name={`pp${student.id}`}
                     value={student.pp}
@@ -615,7 +410,7 @@ export const TableSaveCalificationStudent = ({
                       setAttendance({ ...student, pp: e.target.value })
                     }
                   />
-                </LabelU>
+                </LabelInput>
               </TdInput>
               <TdInput
                 style={{
@@ -626,11 +421,8 @@ export const TableSaveCalificationStudent = ({
                   }`,
                 }}
               >
-                <LabelU
-                  style={{ justifyContent: "center", fontSize: "initial" }}
-                >
+                <LabelInput>
                   <input
-                    style={{ marginInlineStart: "0" }}
                     type="text"
                     name={`pe${student.id}`}
                     value={student.pe}
@@ -638,7 +430,7 @@ export const TableSaveCalificationStudent = ({
                       setAttendance({ ...student, pe: e.target.value })
                     }
                   />
-                </LabelU>
+                </LabelInput>
               </TdInput>
             </Tr>
           ))
--- src/pages/Teacher/components/TableData/styled-components/TableData.style.ts
+++ src/pages/Teacher/components/TableData/styled-components/TableData.style.ts
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { Label } from "../../Login/styled-components";
 
 const Table = styled.table`
   inline-size: 100%;
@@ -9,6 +10,7 @@ const Table = styled.table`
 const Tr = styled.tr`
   &:hover {
     background-color: #1567ff;
+    cursor: pointer;
   }
 `;
 const Th = styled.th`
@@ -25,6 +27,10 @@ const Td = styled.td`
   text-transform: capitalize;
 `;
 
+const TdInput = styled(Td)`
+  max-inline-size: 4ch;
+`;
+
 const LinkData = styled(Link)`
   inline-size: 100%;
   block-size: 100%;
@@ -33,4 +39,54 @@ const LinkData = styled(Link)`
   color: #fff;
 `;
 
-export { LinkData, Table, Td, Th, Tr };
+const LabelTD = styled(Td)<{ val?: number }>`
+  & label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    inline-size: 100%;
+    block-size: 1.5rem;
+    cursor: pointer;
+  }
+  & label::before {
+    content: "";
+    inline-size: 1.25rem;
+    block-size: 1.25rem;
+    border: 1px solid #1567ff;
+    border-radius: 0.25rem;
+  }
+  & label input {
+    display: none;
+  }
+  &:has(:checked) {
+    label::before {
+      display: none;
+    }
+    ${(props) => {
+      switch (props.val) {
+        case 2:
+          return css`
+            background-color: #008000;
+          `;
+        case 0:
+          return css`
+            background-color: #ea0000;
+          `;
+        case 1:
+          return css`
+            background-color: #ffcc0f;
+          `;
+      }
+    }}
+  }
+`;
+const LabelInput = styled(Label)`
+  justify-content: center;
+  font-size: initial;
+  & input {
+    border: none;
+    text-align: center;
+    margin-inline-start: 0;
+  }
+`;
+export { LabelTD, LinkData, Table, Td, TdInput, Th, Tr, LabelInput };
