de483a51036b18804211b668bde7b69810c7eb50
cristianaquino <ycristian10@gmail.com>
2024-04-08 14:50:12 -0500

    feat: routes application

--- src/pages/Teacher/components/Dashboard/Dashboard.tsx
+++ src/pages/Teacher/components/Dashboard/Dashboard.tsx
@@ -4,18 +4,38 @@ import { RoutesWithNotFound } from "@app/guards";
 import { PROYECTS_ROUTE } from "@app/routes";
 import { lazy } from "react";
 import { Route } from "react-router-dom";
-import {
-  Course,
-  DashboardLayout,
-  ListCourse,
-  Profile,
-  RegisterCourse,
-  Student,
-  UploadSheet,
-  UpdateTeacher,
-} from "..";
+import { DashboardLayout } from "..";
 
+// home
 const Home = lazy(() => import("./Home/Home"));
+// teacher
+const Profile = lazy(() => import("../Profile/Profile"));
+const UpdateProfile = lazy(() => import("../UpdateProfile/UpdateProfile"));
+// course
+const RegisterCourse = lazy(() => import("../RegisterCourse/RegisterCourse"));
+const ActionsInCourses = lazy(
+  () => import("../ActionsInCourses/ActionsInCourses")
+);
+const InfoCourse = lazy(() => import("../InfoCourse/InfoCourse"));
+// student
+const Student = lazy(() => import("../Student/Student"));
+const RegisterStudents = lazy(
+  () => import("../RegisterStudents/RegisterStudents")
+);
+// calification
+const RegisterCalification = lazy(
+  () => import("../RegisterCalification/RegisterCalification")
+);
+const SaveCalificationCourse = lazy(
+  () => import("../SaveCalificationCourse/SaveCalificationCourse")
+);
+// attendance
+const RegisterAttendance = lazy(
+  () => import("../RegisterAttendance/RegisterAttendance")
+);
+const TakeAttendanceCourse = lazy(
+  () => import("../TakeAttendanceCourse/TakeAttendanceCourse")
+);
 
 export type DashboardProps = {
   // types...
@@ -26,15 +46,27 @@ const Dashboard = ({}: DashboardProps) => {
       <Route path={`${PROYECTS_ROUTE.HOME}`} element={<DashboardLayout />}>
         <Route path="/" element={<Home />}>
           <Route index element={<p>Ninguna accion realizada</p>} />
-          <Route path="me/profile" element={<Profile />} />
-          <Route path="me/update" element={<UpdateTeacher />} />
-          <Route path="course/register" element={<RegisterCourse />} />
-          <Route path="course/list" element={<ListCourse />} />
-          <Route path="calification/register" element={<RegisterCourse />} />
-          <Route path="student/register" element={<UploadSheet />} />
-          <Route path="attendance/register" element={<RegisterCourse />} />
-          <Route path="course/info/:id" element={<Course />} />
-          <Route path="student/info/:id" element={<Student />} />
+          <Route path="me">
+            <Route path="profile" element={<Profile />} />
+            <Route path="update" element={<UpdateProfile />} />
+          </Route>
+          <Route path="course">
+            <Route path="register" element={<RegisterCourse />} />
+            <Route path="list" element={<ActionsInCourses />} />
+            <Route path="info/:id" element={<InfoCourse />} />
+          </Route>
+          <Route path="student">
+            <Route path="register" element={<RegisterStudents />} />
+            <Route path="info/:id" element={<Student />} />
+          </Route>
+          <Route path="attendance">
+            <Route path="search" element={<RegisterAttendance />} />
+            <Route path="search/:id" element={<TakeAttendanceCourse />} />
+          </Route>
+          <Route path="calification">
+            <Route path="search" element={<RegisterCalification />} />
+            <Route path="search/:id" element={<SaveCalificationCourse />} />
+          </Route>
         </Route>
       </Route>
     </RoutesWithNotFound>
