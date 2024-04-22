cb09928c14814e06997b70e65557cb379db4142e
cristianaquino <ycristian10@gmail.com>
2024-04-21 20:07:10 -0500

    feat: update stadium players

--- src/pages/Futball/components/Stadium/Stadium.tsx
+++ src/pages/Futball/components/Stadium/Stadium.tsx
@@ -1,17 +1,212 @@
 "use client";
 
-import { Cancha } from "./styled-components";
+import { useEffect, useState } from "react";
+import {
+  Banda,
+  Cancha,
+  Content,
+  ItemPlayer,
+  ListPlayer,
+  Player,
+} from "./styled-components";
 
 export type StadiumProps = {
   // types...
 };
 
 const Stadium = ({}: StadiumProps) => {
+  const [formation, setFormation] = useState("");
+  const [hijos, setHijos] = useState<any>([]);
+  const [col, setCol] = useState(0);
+  const [{ team, players }, setPlayers] = useState<any>([]);
+  const n = Array.from({ length: 11 }).fill(
+    "https://i.postimg.cc/V6zbW55L/blank-profile-picture.png"
+  ) as string[];
+  const [urls, setUrl] = useState<string[]>(n);
+
+  function handleFormation(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    const rgx = /^([1-9]|-)+/gi;
+    const m = value.match(rgx) || [""];
+
+    if (m[0].charAt(0) === "-") {
+      return setFormation("");
+    }
+
+    if (!m[0].includes("-")) {
+      const a = m[0].split("");
+      const t = a.reduce((acc, curr) => {
+        acc += parseInt(curr);
+        return acc;
+      }, 0);
+      if (t > 10) {
+        return setFormation(m[0].slice(0, m[0].lastIndexOf("-")));
+      } else {
+        return setFormation(a.join("-"));
+      }
+    }
+
+    if (m[0].includes("-")) {
+      let t = m[0].split("-").reduce((acc, curr) => {
+        acc += parseInt(curr);
+        return acc;
+      }, 0);
+
+      if (t > 10) {
+        return setFormation(m[0].slice(0, m[0].lastIndexOf("-")));
+      } else {
+        setCol(m[0].split("-").length - 1);
+        return setFormation(m[0]);
+      }
+    }
+  }
+
+  function handleClick() {
+    let n = 2;
+    const l = formation.split("-");
+    const check = [];
+    for (let i = 0; i <= col; i++) {
+      check.push(<Ja n={parseInt(l[i])} p={n} urls={urls} players={players} />);
+      n = n + parseInt(l[i]);
+    }
+    setHijos(check);
+  }
+
+  useEffect(() => {
+    async function getPlayers() {
+      const url = await fetch("/data.json");
+      const data = await url.json();
+      setPlayers(data);
+    }
+    getPlayers();
+  }, []);
+
+  useEffect(() => {
+    const li = document.querySelectorAll("label");
+    function ac(this: any) {
+      li.forEach((e) => {
+        e.classList.remove("ja");
+      });
+      this.classList.add("ja");
+    }
+    li.forEach((e) => {
+      e.addEventListener("click", ac);
+    });
+    return () => {
+      removeEventListener("click", ac);
+    };
+  }, [hijos]);
+
+  function handleChange({ photo, pos }: { photo: string; pos: number }) {
+    const nuevo = [...urls].map((e, i) => {
+      if (i === pos) {
+        return photo;
+      }
+      return e;
+    });
+    setUrl(nuevo);
+  }
+
   return (
-    <div style={{ display: "flex", placeContent: "center" }}>
-      <Cancha />
-    </div>
+    <>
+      <input type="text" value={formation} onChange={handleFormation} />
+      <button onClick={handleClick}>click</button>
+      <p>{team?.name}</p>
+      <img src={team?.logo} alt="" />
+
+      <div style={{ display: "flex", placeContent: "center" }}>
+        <Cancha>
+          {hijos.length > 0 && (
+            <>
+              <Banda>
+                <Content>
+                  <Player>
+                    <img
+                      src={urls[0]}
+                      alt=""
+                      style={{
+                        width: "4.5rem",
+                        height: "4.5rem",
+                        borderRadius: "50%",
+                      }}
+                    />
+                  </Player>
+                  <ListPlayer>
+                    {players?.map((player: any) => (
+                      <ItemPlayer
+                        key={player.id}
+                        onClick={() =>
+                          handleChange({ photo: player.photo, pos: 0 })
+                        }
+                      >
+                        <img src={player.photo} />
+                        {player?.name}
+                      </ItemPlayer>
+                    ))}
+                  </ListPlayer>
+                </Content>
+              </Banda>
+              {hijos}
+            </>
+          )}
+        </Cancha>
+      </div>
+    </>
   );
 };
 
+const Ja = ({
+  n,
+  p,
+  urls,
+  players,
+}: {
+  n: number;
+  p: number;
+  urls: string[];
+  players: string[];
+}) => {
+  const [ja, setJa] = useState(urls.slice(0, n));
+  function handleChange({ photo, pos }: { photo: string; pos: number }) {
+    const nuevo = [...ja].map((e, i) => {
+      if (i === pos) {
+        return photo;
+      }
+      return e;
+    });
+    setJa(nuevo);
+  }
+  return (
+    <Banda>
+      {Array.from({ length: n }).map((_, index) => (
+        <Content key={p + index}>
+          <Player>
+            <img
+              src={ja[index]}
+              alt=""
+              style={{
+                width: "4.5rem",
+                height: "4.5rem",
+                borderRadius: "50%",
+              }}
+            />
+          </Player>
+          <ListPlayer>
+            {players?.map((player: any) => (
+              <ItemPlayer
+                key={player.id}
+                onClick={() =>
+                  handleChange({ photo: player.photo, pos: index })
+                }
+              >
+                <img src={player.photo} />
+                {player?.name}
+              </ItemPlayer>
+            ))}
+          </ListPlayer>
+        </Content>
+      ))}
+    </Banda>
+  );
+};
 export default Stadium;
--- src/pages/Futball/components/Stadium/styled-components/Stadium.style.ts
+++ src/pages/Futball/components/Stadium/styled-components/Stadium.style.ts
@@ -57,12 +57,6 @@ const Cancha = styled.div`
       white 90px 92px,
       transparent 92px 100%
     ),
-    /* right area */ linear-gradient(to left, transparent 98%, white 98%) 100%
-      50%/165px 403.2px no-repeat,
-    linear-gradient(to bottom, transparent 99%, white 99%) 100% 50%/165px
-      403.2px no-repeat,
-    linear-gradient(to top, transparent 99%, white 99%) 100% 50%/165px 403.2px
-      no-repeat,
     /* littel area */ linear-gradient(to left, transparent 98%, white 98%) 100%
       50%/55px 183.2px no-repeat,
     linear-gradient(to bottom, transparent 99%, white 99%) 100% 50%/55px 183.2px
@@ -75,6 +69,18 @@ const Cancha = styled.div`
         white 0 4px,
         transparent 4px
       ),
+    /* right area */ linear-gradient(to bottom, transparent 99%, white 99%) 100%
+      50%/165px 403.2px no-repeat,
+    linear-gradient(to top, transparent 99%, white 99%) 100% 50%/165px 403.2px
+      no-repeat,
+    linear-gradient(to left, #268e00 73%, #1e7200 73% 99%, white 98%) 100% 50%/165px
+      403.2px no-repeat,
+    radial-gradient(
+      circle 4px at calc(100% - 110px) 50%,
+      transparent 90px,
+      white 90px 92px,
+      transparent 92px 100%
+    ),
     /* center */
       radial-gradient(
         circle,
@@ -87,4 +93,79 @@ const Cancha = styled.div`
       repeating-linear-gradient(to right, #0003 0 10%, transparent 10% 20%),
     linear-gradient(to right, #268e00 100%, transparent);
 `;
-export { Cancha };
+
+const Banda = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Player = styled.label`
+  --w: 4.5rem;
+  display: inline-block;
+  inline-size: var(--w);
+  block-size: var(--w);
+  border-radius: 50%;
+  border: 1px solid white;
+  input {
+    display: none;
+  }
+`;
+const ListPlayer = styled.ul`
+  block-size: 0;
+  inline-size: 200px;
+  overflow: hidden;
+  overflow-y: scroll;
+  transition: block-size 0.3s ease-in-out;
+  position: absolute;
+  inset-block-start: 4.5rem;
+  inset-inline-end: 0;
+  padding: 0 0.5rem;
+`;
+const ItemPlayer = styled.li`
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  padding: 0.5rem 0;
+  border-bottom: 1px solid white;
+
+  &:last-child {
+    border-bottom: none;
+  }
+
+  img {
+    inline-size: 2.5rem;
+    block-size: 2.5rem;
+    border-radius: 50%;
+  }
+`;
+
+const Content = styled.div`
+  position: relative;
+  block-size: auto;
+
+  label.ja + ul {
+    block-size: 120px;
+    transition-duration: 1s;
+    border: 1px solid white;
+    border-radius: 0.5rem;
+    background-color: #000a;
+    scrollbar-width: none;
+    scrollbar-color: white #000a;
+  }
+  /* &:has(:checked) {
+    ${ListPlayer} {
+      block-size: 120px;
+      transition-duration: 1s;
+      border: 1px solid white;
+      border-radius: 0.5rem;
+      background-color: #000a;
+      scrollbar-width: none;
+      scrollbar-color: white #000a;
+    }
+  } */
+`;
+
+export { Banda, Cancha, Content, ItemPlayer, ListPlayer, Player };
