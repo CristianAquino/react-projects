61bd7154c772f8d419e94fc54ac77f4de8b555f6
cristianaquino <ycristian10@gmail.com>
2024-04-23 12:06:38 -0500

    feat: barrel

--- src/pages/Futball/components/index.ts
+++ src/pages/Futball/components/index.ts
@@ -1,3 +1,7 @@
+export * from "./AlineationPlayer";
+export * from "./Club";
 export * from "./Home";
+export * from "./InputAlineation";
 export * from "./Main";
 export * from "./Stadium";
+export * from "./Team";
