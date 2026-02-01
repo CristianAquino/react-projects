aec171bf939fdefb5f4f3b1826dc2220331da6d0
cristianaquino <ycristian10@gmail.com>
2026-02-01 15:18:52 -0500

    .

--- src/pages/Football/components/FCCard/FCCard.tsx
+++ src/pages/Football/components/FCCard/FCCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { FaStar } from "react-icons/fa";
 import {
   Attribute,
@@ -13,6 +13,8 @@ import {
   RightCenter,
   TopLeft,
 } from "./styled-components";
+
+import html2canvas from "html2canvas";
 
 export type FCCardProps = {
   // types...
@@ -64,76 +66,92 @@ const FCCard = ({
     content.querySelector("g")?.setAttribute("fill", card.card.color);
   }, [card, chemistry]);
 
+  const refDown = useRef<any>();
+
+  async function handleDonwload() {
+    const canvas = await html2canvas(refDown.current, {
+      backgroundColor: null,
+    });
+    const imgData = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = imgData;
+    link.download = "card.png";
+    link.click();
+  }
+
   return (
-    <Box>
-      <img src={card?.card.image} alt="upscale-team_of_the_year" />
-      <TopLeft className="chemistry">
-        <span>{overall}</span>
-        <span>{position}</span>
-        {chemistry}
-      </TopLeft>
-      <LeftCenter className="svgContent">
-        {playStyle.map((ele) => {
-          if (typeof ele === "function") return ele();
-          return ele;
-        })}
-      </LeftCenter>
-      <RightCenter>
-        <div>
-          <p>skill</p>
-          <p>
-            {skill.card.name} <FaStar />
-          </p>
-        </div>
-        <div>
-          <p>weak</p>
-          <p>
-            {weak.card.name} <FaStar />
-          </p>
-        </div>
-        <div>
-          <p>work</p>
-          <p>{work}</p>
-        </div>
-      </RightCenter>
-      <Bottom>
-        <p>{player}</p>
-        <Attributes>
-          <Attribute>
-            <span>PAC</span>
-            <span>{att[0]}</span>
-          </Attribute>
-          <Attribute>
-            <span>SHO</span>
-            <span>{att[1]}</span>
-          </Attribute>
-          <Attribute>
-            <span>PAS</span>
-            <span>{att[2]}</span>
-          </Attribute>
-          <Attribute>
-            <span>DRI</span>
-            <span>{att[3]}</span>
-          </Attribute>
-          <Attribute>
-            <span>DEF</span>
-            <span>{att[4]}</span>
-          </Attribute>
-          <Attribute>
-            <span>PHY</span>
-            <span>{att[5]}</span>
-          </Attribute>
-        </Attributes>
-        <Flags>
-          <img src="/data/FIFA/COUNTRY/england.png" alt="" />
-          <img src="/data/FIFA/LEAGUE/pl.png" alt="" />
-          <img src="/data/FIFA/TEAM/mu.png" alt="" />
-        </Flags>
-      </Bottom>
-      <Player>
-        <img src={image} alt="" className="user" />
-      </Player>
-    </Box>
+    <>
+      <Box ref={refDown}>
+        <img src={card?.card.image} alt="upscale-team_of_the_year" />
+        <TopLeft className="chemistry">
+          <span>{overall}</span>
+          <span>{position}</span>
+          {chemistry}
+        </TopLeft>
+        <LeftCenter className="svgContent">
+          {playStyle.map((ele) => {
+            if (typeof ele === "function") return ele();
+            return ele;
+          })}
+        </LeftCenter>
+        <RightCenter>
+          <div>
+            <p>skill</p>
+            <p>
+              {skill.card.name} <FaStar />
+            </p>
+          </div>
+          <div>
+            <p>weak</p>
+            <p>
+              {weak.card.name} <FaStar />
+            </p>
+          </div>
+          <div>
+            <p>work</p>
+            <p>{work}</p>
+          </div>
+        </RightCenter>
+        <Bottom>
+          <p>{player}</p>
+          <Attributes>
+            <Attribute>
+              <span>PAC</span>
+              <span>{att[0]}</span>
+            </Attribute>
+            <Attribute>
+              <span>SHO</span>
+              <span>{att[1]}</span>
+            </Attribute>
+            <Attribute>
+              <span>PAS</span>
+              <span>{att[2]}</span>
+            </Attribute>
+            <Attribute>
+              <span>DRI</span>
+              <span>{att[3]}</span>
+            </Attribute>
+            <Attribute>
+              <span>DEF</span>
+              <span>{att[4]}</span>
+            </Attribute>
+            <Attribute>
+              <span>PHY</span>
+              <span>{att[5]}</span>
+            </Attribute>
+          </Attributes>
+          <Flags>
+            <img src="/data/FIFA/COUNTRY/england.png" alt="" />
+            <img src="/data/FIFA/LEAGUE/pl.png" alt="" />
+            <img src="/data/FIFA/TEAM/mu.png" alt="" />
+          </Flags>
+        </Bottom>
+        <Player>
+          <img src={image} alt="" className="user" />
+        </Player>
+      </Box>
+      <button onClick={handleDonwload}>download</button>
+    </>
   );
 };
 
